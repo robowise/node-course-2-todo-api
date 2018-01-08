e70d65cd8e2ea87f3e420fca9e1543093a0bfeb8
F H <fouadhusseini@hotmail.com>
2018-01-08 07:18:40 +0000

    init commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,54 @@
+//const MongoClient = require('mongodb'). MongoClient;
+
+const {MongoClient, ObjectID} = require('mongodb');
+
+/*
+var obj = ObjectID();
+console.log(obj);
+*/
+
+/*
+var user = {name: 'fouad', age: 25};
+
+var {name, age} =user;
+console.log(age, name);
+*/
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err){
+      return  console.log('unable to connect to mongodb server');
+    }
+    console.log('connected to mongodb server');
+
+    db.collection('Todos').insertOne({
+        text: 'walk the dog',
+        completed: true
+    }, (err, result) => {
+    if (err) {
+       return console.log('unable to insert Todo', err);
+    }
+    
+    console.log(JSON.stringify(result.ops, undefined, 2));
+    
+    });
+
+
+    db.collection('Users').insertOne({
+        _id: 1234,
+        name: 'fouad',
+        age: 50,
+        location: 'khobar'
+    }, (err, result) => {
+        if (err) {
+          return  console.log('unable to insert user', err);
+        }
+        
+        console.log(result.ops[0]._id.getTimestamp());
+        
+    });
+    
+
+    db.close();
+});
+
+
---
+++ playground/mongodb-find.js
@@ -0,0 +1,53 @@
+//const MongoClient = require('mongodb'). MongoClient;
+
+const {MongoClient, ObjectID} = require('mongodb');
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err){
+      return  console.log('unable to connect to mongodb server');
+    }
+    console.log('connected to mongodb server');
+
+    /*
+    db.collection('Todos')
+    .find({_id: new ObjectID('5a5203487c16ab10e1d08d66')})
+    .toArray().then((docs) => {
+    console.log('Todos');
+    console.log(JSON.stringify(docs, 0, 2));
+    }, (err) => {
+        console.log('unable to find Todos, err');
+        });
+    */  
+    
+     db.collection('Todos')
+    .find({text: 'walk the dog'})
+    .count()
+    .then((docs) => {
+    
+    console.log(docs);
+    }, (err) => {
+        console.log('unable to find Todos, err');
+        });
+        
+    /*    
+     db.collection('Todos')
+    .find()
+    //.toArray()
+    .count()
+    .then((count) => {
+    console.log(`Todos: ${count}`);
+     
+    //console.log(JSON.stringify(count, 0, 2));
+    }, (err) => {
+        console.log('unable to find Todos, err');
+        });
+    */    
+        
+    db.close();
+});
+    
+
+
+
+////    ./mongod --dbpath ~/workspace/mongo-data
