03af35e48a28729ba8f609fcca4d92ed6f4bdccc
F H <fouadhusseini@hotmail.com>
2018-01-14 09:23:56 +0000

    add GET todos route

--- server/server.js
+++ server/server.js
@@ -7,27 +7,31 @@ var {Users} = require("./models/user");
 
 var app = express();
 
-
-
 app.use(bodyParser.json());
-
+//..//
 app.post('/todos', (req, res) => {
     var todo = new Todo({
         text: req.body.text
     });
 todo.save().then((doc) => {
     res.send(doc);
-    
 }, (e) => {
     res.status(400).send(e);
-    
 });
 });
+//..//
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+
 
 app.listen(8080, () => {
     console.log("started on port 8080");
-    
-
 });
 
 module.exports = {app};
